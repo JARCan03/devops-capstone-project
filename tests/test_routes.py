c9d10e03c60de85fed5ce02f3ff35cdfe9109af3
JARCan03 <thefrantier911@gmail.com>
2025-07-10 07:10:35 -0400

    Added the Read Account Feature with test routes

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -124,3 +124,19 @@ class TestAccountService(TestCase):
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
     # ADD YOUR TEST CASES HERE ...
+    def test_read_an_account(self):
+        """It should Read a single Account"""
+        account = self._create_accounts(1)[0]
+        respo = self.client.get(
+            f"{BASE_URL}/{account.id}", content_type="application/json"
+        )
+        self.assertEqual(respo.status_code, status.HTTP_200_OK)
+        data = respo.get_json()
+        self.assertEqual(data["name"], account.name)
+        #checks to see if name is provided is the same
+    
+    def test_account_not_found(self):
+        """It should test if account doesn't exist"""
+        respo = self.client.get(f"{BASE_URL}/0")
+        self.assertEqual(respo.status_code, status.HTTP_404_NOT_FOUND)
+        #checks to see that account is indeed not found
